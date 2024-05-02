b25f188da33cdada1dfd5c7db7f0158672789b21
jaggyz <jaggyzmail@yahoo.com>
2024-05-02 15:50:16 +0700

    [Edited] working with string and number

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,15 +1,58 @@
 print("Hello World")
-print("")
+print("\n")
 
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
-print("")
+print("\n")
 
+print("Working with variables")
 character_name = "John"
 character_age = "35"
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+print("\n")
+
+print("Working with strings")
+phrase = "We Love Bug"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("W"))
+print(phrase.index("Lov"))
+print(phrase.replace("We","You"))
+print("\n")
+
+print("Working with numbers")
+print(-2.0987)
+print(3 + 4.5)
+print(3 * 4 + 5)
+print(10 % 3)
+my_num = 5
+print(str(my_num) + " is my favorite number")
+my_num = -5
+print(abs(my_num))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(round(3.7))
+
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+
+
+
+
+
+
+
+
+
